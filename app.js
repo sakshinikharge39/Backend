b10dd98dc549fdfc181a48c25483d98b2fc97150
sakshinikharge39 <srnikharge7788@gmail.com>
2024-04-16 20:15:33 +0530

    before routes

---
+++ app.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+dotenv.config({ path: "./config/config.env" });
+
+app.use(
+  cors({
+    origin: [process.env.FRONTEND_URL],
+    methods: ["GET", "POST", "DELETE", "PUT"],
+    Credentials: true,
+  })
+);
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(
+  fileUpload({
+    useTempFiles: true,
+    tempFileDir: "/tmp/",
+  })
+);
+
+
+export default app;
