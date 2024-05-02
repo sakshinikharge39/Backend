1005c188ea474c6123d2979461c44555dd2351e3
sakshinikharge39 <srnikharge7788@gmail.com>
2024-05-03 00:58:07 +0530

    user created in backend

--- middlewares/error.js
+++ middlewares/error.js
@@ -25,7 +25,7 @@ export const errorMiddleware = (err, req, res, next) => {
     const message = `Json Web Token is Expired. Try Again`;
     err = new ErrorHandler(message, 400);
   }
-  return res.status(statusCode).json({
+  return res.status(err.statusCode).json({
     success: false,
     message: err.message,
   });
--- models/userSchema.js
+++ models/userSchema.js
@@ -54,3 +54,5 @@ userSchema.methods.comparePassword = async function(enteredPassword){
 userSchema.methods.getJWTToken = function(){
     return jwt.sign({id: this._id}, process.env.JWT_SECRET_KEY, {expireIn: process.env.JWT_EXPIRE})
 }
+
+export const User = mongoose.model("User", userSchema);
