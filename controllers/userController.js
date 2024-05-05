733a1074b5364c287ded0a55e3c251a8f018864a
sakshinikharge39 <srnikharge7788@gmail.com>
2024-05-05 11:43:54 +0530

    login succefully

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,25 +1,47 @@
-import {catchAsyncError} from '../middlewares/catchAsyncError.js'
-import ErrorHandler from '../middlewares/error.js';
-import {User} from '../models/userSchema.js'
-export const register = catchAsyncError(async(req, res, next) => {
-    const {name, email, phone, role, password} = req.body;
-    if(!name || !email|| !phone || !role||  !password){
-        return next(new ErrorHandler("please fill registration form"))
-    }
-    const isEmail = await User.findOne({email});
-    if (isEmail){
-        return next (new ErrorHandler("Email already exist"))
-    }
-    const user = await User.create({
-        name, 
-        email, 
-        phone, 
-        role, 
-        password,
-    });
-    res.status(200).json({
-        success: true,
-        message: "User Rgistered!",
-        user,
-    })
+import { catchAsyncError } from "../middlewares/catchAsyncError.js";
+import ErrorHandler from "../middlewares/error.js";
+import { User } from "../models/userSchema.js";
+import { sendToken } from "../utils/jwtToken.js";
+
+export const register = catchAsyncError(async (req, res, next) => {
+  const { name, email, phone, role, password } = req.body;
+  if (!name || !email || !phone || !role || !password) {
+    return next(new ErrorHandler("please fill registration form"));
+  }
+  const isEmail = await User.findOne({ email });
+  if (isEmail) {
+    return next(new ErrorHandler("Email already exist"));
+  }
+  const user = await User.create({
+    name,
+    email,
+    phone,
+    role,
+    password,
+  });
+  // res.status(200).json({
+  //     success: true,
+  //     message: "User Rgistered!",
+  //user,
+  // })
+  sendToken(user, 200, res, "User Registered Succefully!");
+});
+
+export const login = catchAsyncError(async (req, res, next) => {
+  const { email, password, role } = req.body;
+  if (!email || !password || !role) {
+    return next(new ErrorHandler("please fill login form.", 400));
+  }
+  const user = await User.findOne({ email }).select("+password");
+  if (!user) {
+    return next(new ErrorHandler("Invalid Email or Password", 400));
+  }
+  const isPasswordMatched = await user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Email or password", 400));
+  }
+  if (user.role !== role) {
+    return next(new ErrorHandler("User with his role not found", 400));
+  }
+  sendToken(user, 200, res, "User Logged in succesfully!");
 });
--- models/userSchema.js
+++ models/userSchema.js
@@ -1,58 +1,61 @@
-import mongoose from 'mongoose';
-import validator from 'validator';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-// import { type } from 'express/lib/response';
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 
 const userSchema = new mongoose.Schema({
-    name:{
-        type: String,
-        required: true,
-        minLength: [3, "Name must contain at least 3 characters!"],
-        maxLength: [30, "Name cannot exceed 30 charcters!"],
-    },
-    email:{
-        type: String,
-        required: [true, "Email is required"],
-        validate: [validator.isEmail, "please provide a valid Email!"],
-    },
-    phone:{
-        type: Number,
-        required: [true, "Phone number is required"]
-    },
-    password:{
-        type:String,
-        required: [true, "password is required"],
-        minLength: [8, "Password must contain atleast 8 characters!"],
-        maxLength: [32, "password cannot exceed 32 characters!"],
-    },
-    role:{
-        type: String,
-        required: [true, "Please provide your role"],
-        enum: ["Job Seeker", "Employer"],
-    },
-    createdAt:{
-        type : Date,
-        default: Date.now,
-    },
+  name: {
+    type: String,
+    required: [true, "Please enter your Name!"],
+    minLength: [3, "Name must contain at least 3 Characters!"],
+    maxLength: [30, "Name cannot exceed 30 Characters!"],
+  },
+  email: {
+    type: String,
+    required: [true, "Please enter your Email!"],
+    validate: [validator.isEmail, "Please provide a valid Email!"],
+  },
+  phone: {
+    type: Number,
+    required: [true, "Please enter your Phone Number!"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a Password!"],
+    minLength: [8, "Password must contain at least 8 characters!"],
+    maxLength: [32, "Password cannot exceed 32 characters!"],
+    select: false,
+  },
+  role: {
+    type: String,
+    required: [true, "Please select a role"],
+    enum: ["Job Seeker", "Employer"],
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-//HASHING THE PASSWORD 
-userSchema.pre("save", async function(next){
-    if(!this.isModified("password")){
-        next()
-    }
-    this.password = await bcrypt.hash(this.password, 10);
+//ENCRYPTING THE PASSWORD WHEN THE USER REGISTERS OR MODIFIES HIS PASSWORD
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
 });
 
-//COMPARING PASSWORD
-userSchema.methods.comparePassword = async function(enteredPassword){
-    return await bcrypt.compare(enteredPassword, this.password);
+//COMPARING THE USER PASSWORD ENTERED BY USER WITH THE USER SAVED PASSWORD
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
 };
 
-// GENERATING A JWT TOKEN FOR AUTHORIZATION
-userSchema.methods.getJWTToken = function(){
-    return jwt.sign({id: this._id}, process.env.JWT_SECRET_KEY, {expireIn: process.env.JWT_EXPIRE})
-}
+//GENERATING A JWT TOKEN WHEN A USER REGISTERS OR LOGINS, IT DEPENDS ON OUR CODE THAT WHEN DO WE NEED TO GENERATE THE JWT TOKEN WHEN THE USER LOGIN OR REGISTER OR FOR BOTH.
+userSchema.methods.getJWTToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET_KEY, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
 
 export const User = mongoose.model("User", userSchema);
